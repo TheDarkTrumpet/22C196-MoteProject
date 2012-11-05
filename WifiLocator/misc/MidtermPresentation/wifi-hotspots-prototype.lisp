b111eaac0bdf0724001df208014d720cbab361c7
David Thole <dthole@gmail.com>
2012-11-04 20:02:03 -0600

    Implemented a function to load the wifi-formatted data, and returns an alist.  This function does a little too much, and is a bit ugly...but works, may reformat later

--- WifiLocator/misc/MidtermPresentation/wifi-hotspots-prototype.lisp
+++ WifiLocator/misc/MidtermPresentation/wifi-hotspots-prototype.lisp
@@ -14,3 +14,23 @@
   (:use :cl :cl-ppcre))
 
 (in-package :wifi-hotspots-prototype)
+
+
+;;;;; Loading the formatted file ;;;;;;;
+
+(defun load-wifi-formatted-file (file)
+  "Loads the file passed in, returns an ALIST of WIFI mac addresses, and the abs signal strength"
+  (let ((mac-addr-db '()))
+    (with-open-file (f file :direction :input)
+      (loop for l = (read-line f nil nil)
+	    for l-split = (split "\\\t" l)
+	    for mac-addr = (first l-split)
+	    for signal-str = (second l-split)
+	    while l
+	    do
+	       (if (null (cdr (assoc mac-addr mac-addr-db :test #'string-equal)))
+		   (push (cons mac-addr (list (abs (read-from-string signal-str))))
+			 mac-addr-db)
+		   (push (abs (read-from-string signal-str))
+			 (cdr (assoc mac-addr mac-addr-db :test #'string-equal))))))
+    mac-addr-db))
