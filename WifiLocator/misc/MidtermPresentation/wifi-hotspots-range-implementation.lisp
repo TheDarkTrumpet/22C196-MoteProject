4305e988c8763459f39e362687b4267269441e7a
David Thole <dthole@gmail.com>
2012-11-13 17:23:09 -0600

    Initial commit of the range implementation.  This will be the prototype that eventually makes it into our project

---
+++ WifiLocator/misc/MidtermPresentation/wifi-hotspots-range-implementation.lisp
@@ -0,0 +1,13 @@
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (progn 
+    (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+					   (user-homedir-pathname))))
+      (when (probe-file quicklisp-init)
+	(load quicklisp-init)))
+    (require :cl-ppcre)))
+
+(defpackage :wifi-classifier-prototype
+  (:use :cl :cl-ppcre))
+
+(in-package :wifi-hotspots-prototype)
+
