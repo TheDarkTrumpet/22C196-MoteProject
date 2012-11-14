3ba3f22db131c01cc894cae065d72c38a0f387d9
David Thole <dthole@gmail.com>
2012-11-14 16:18:32 -0600

    Adding an if statement to detect our underlying operating system, since gsed is necessary on OSX to actually correct deal with these files

--- WifiLocator/misc/MidtermPresentation/data/format_data.sh
+++ WifiLocator/misc/MidtermPresentation/data/format_data.sh
@@ -5,6 +5,13 @@
 
 echo "Converting the wifiData*.csv into a set where I have MAC_ADDR\tSIGNAL for easier processing downroad"
 
-ls wifiData_*.csv | sed -e 's/\.csv$//' | grep -v 'reformatted' | xargs -I{} sh -c "grep 'eduroam' {}.csv | sed -E 's/,/\t/g' | cut -f 2,3 > {}.reformatted.csv"
+MYCLI=`uname`
+
+if [[ "$MYCLI" == "Darwin" ]]
+then
+    ls wifiData_*.csv | sed -e 's/\.csv$//' | grep -v 'reformatted' | xargs -I{} sh -c "grep 'eduroam' {}.csv | gsed -E 's/,/\t/g' | cut -f 2,3 > {}.reformatted.csv"
+else
+    ls wifiData_*.csv | sed -e 's/\.csv$//' | grep -v 'reformatted' | xargs -I{} sh -c "grep 'eduroam' {}.csv | sed -E 's/,/\t/g' | cut -f 2,3 > {}.reformatted.csv"
+fi
 
 echo "That's it for now..."
