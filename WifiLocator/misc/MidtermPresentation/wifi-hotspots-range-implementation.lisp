379c26c2763a8118ee9bd1a54b1f47442ef919db
David Thole <dthole@gmail.com>
2012-11-14 07:18:19 -0600

    Adding the function load-assoc-files, which will take an alist room_name, file_name type setup, and will load all the files into a giant alist.  This can be used to create the ranges later

--- WifiLocator/misc/MidtermPresentation/wifi-hotspots-range-implementation.lisp
+++ WifiLocator/misc/MidtermPresentation/wifi-hotspots-range-implementation.lisp
@@ -11,5 +11,19 @@
 
 (in-package :wifi-classifier-prototype)
 
+; Basic loading of files
+(defun load-assoc-files (filelist)
+  "Given a list of files, in the format of:
+ROOM_NAME . FILE_NAME
+we'll return an ALIST such that it's:
+ (ROOM_NAME . ((MAC_ADDR . '(LIST_OF_ADDRS))
+               (...)))
+"
+  (loop for f in filelist
+	for room_label = (car f)
+	for file = (cdr f)
+	for loaded-file = (load-wifi-formatted-file ffile)
+	while f
+	collect (cons room_label loaded-file)))
 
 
